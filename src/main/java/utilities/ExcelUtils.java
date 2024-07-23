9cf90b9880efd7926aa1161fc651644ba7577026
Vusi Pelo <theautomationworkshop@gmail.com>
2024-07-23 20:58:55 +0200

    Initial commit

---
+++ src/main/java/utilities/ExcelUtils.java
@@ -0,0 +1,43 @@
+package utilities;
+
+
+
+public class ExcelUtils {
+    public static void setExcelFile(String Path) throws Exception {
+
+    }
+
+    public static String getCellData(int RowNum, int ColNum, String SheetName ) throws Exception{
+
+        return null;
+    }
+
+
+
+    public static void setResultLink(String Result,  int RowNum, int ColNum, String SheetName,String LinkPath) throws Exception {
+
+    }
+
+    public static int getRowCount(String SheetName){
+
+     return 1;
+    }
+
+    public static int getRowContains(String sTestCaseName, int colNum,String SheetName) throws Exception{
+       return 1;
+    }
+
+    public static int getTestStepsCount(String SheetName, String sTestCaseID, int iTestCaseStart) throws Exception{
+        return 1;
+    }
+
+
+
+
+
+    public static void setCellData(String Result,  int RowNum, int ColNum, String SheetName) throws Exception    {
+
+
+    }
+
+}
